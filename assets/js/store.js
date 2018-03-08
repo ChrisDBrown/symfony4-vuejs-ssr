ab19ca805ae32fd5938464045f622d5f9350b94d
Vladimir Pak <theratg@gmail.com>
2018-03-08 11:32:13 +0200

    fix absolute url

--- assets/js/store.js
+++ assets/js/store.js
@@ -10,7 +10,7 @@ export default new Vuex.Store({
             if (typeof window === 'undefined') {
                 commit('setNumber', 1234567890);
             } else {
-                axios.get('http://127.0.0.1:8000/number').then(number => {
+                axios.get('/number').then(number => {
                     commit('setNumber', number.data.number);
                 });
             }
